fb60bc89b5f2ce57df6f650af8613af490e08eb9
赵传储 <zc992833886@outlook.com>
2019-09-28 16:54:41 +0800

    修改Foreground，添加DepthScene脚本

---
+++ Unity/Yoroshiku/Assets/Scripts/Biz/Map/Common/DepthScene.cs
@@ -0,0 +1,33 @@
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using ZCore;
+
+namespace Biz.Map {
+    public class DepthScene : CallerBehaviour {
+        //暂不需要指定
+        private Camera targetCamera;
+        //越远值越小
+        public float XMoveSpeed;
+        public float YMoveSpeed;
+
+        private Vector3 targetPos;
+
+        private void Awake() {
+            targetCamera = Camera.main;
+        }
+        private void Start() {
+            targetPos = targetCamera.transform.position;
+        }
+
+        private void Update() {
+            Vector3 offset = targetCamera.transform.position - targetPos;
+            float x = transform.position.x + XMoveSpeed * offset.x;
+            float y = transform.position.y + YMoveSpeed * offset.y;
+            transform.position = new Vector2(x, y);
+            targetPos = targetCamera.transform.position;
+        }
+
+    }
+}
