74dffe0e6ebe61d36abcca3ca0b00bc0f5dd48e0
1 <1>
2019-10-03 16:11:05 +0800

    选关

---
+++ Unity/Yoroshiku/Assets/Scripts/Biz/Chapter/ChapterButton.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using ZCore;
+using UnityEngine;
+using UnityEngine.UI;
+namespace Biz.Chapter {
+
+    [RequireComponent(typeof(Button))]
+    public class ChapterButton : MonoBehaviour  {
+        public Sprite EnabledSprite;
+        public Sprite DisabledSprite;
+        public int MapIndex;
+    }
+
+}
