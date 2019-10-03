34a0a8eb58e1b4102d3a405b484fb1479182644a
赵传储 <zc992833886@outlook.com>
2019-10-03 13:41:16 +0800

    修改动画触发机制

---
+++ Unity/Yoroshiku/Assets/Scripts/Biz/Utils/Component/AnimKiller.cs
@@ -0,0 +1,21 @@
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using ZCore;
+
+public class AnimKiller : CallerBehaviour {
+    private Animator anim;
+
+    void Start() {
+        anim = this.GetComponent<Animator>();
+    }
+
+    void Update() {
+        AnimatorStateInfo info = anim.GetCurrentAnimatorStateInfo(0);
+        if (info.normalizedTime >= 1.0f) {
+            Destroy(this.gameObject);
+        }
+
+    }
+}
